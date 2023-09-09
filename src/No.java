55b26b23e8d646e98d89e48dfbdcf6851e757d90
12116534 <guhrodriguesads@gmail.com>
2023-09-08 21:12:27 -0300

    feat: exemplo lista encadeada

---
+++ src/No.java
@@ -0,0 +1,9 @@
+public class No {
+    public Object elemento;
+    public No prox;
+
+    public No (Object elem){
+        elemento = elem;
+        prox = null;
+    }
+}
---
+++ src/Nodo.java
@@ -0,0 +1,22 @@
+public class Nodo {
+
+    private float dado;
+    private Nodo proximo;
+
+    public float getDado() {
+        return dado;
+    }
+
+    public void setDado(float dado) {
+        this.dado=dado;
+    }
+
+    public Nodo getProximo() {
+        return proximo;
+    }
+    public void setProximo(Nodo proximo) {
+        this.proximo = proximo;
+    }
+//insere no inicio
+
+}
